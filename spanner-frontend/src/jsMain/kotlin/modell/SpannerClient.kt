609f49dbcdb0b1c3b23eef6cd48bd34ec0d20891
Oscar Vladau-Husevold <oscar.vladau-husevold@nav.no>
2021-08-04 12:50:57 +0200

    Fjerne reduntant Host-klasse
    Co-authored-by: Peter Stensby <peter.stensby@nav.no>

--- spanner-frontend/src/jsMain/kotlin/modell/SpannerClient.kt
+++ spanner-frontend/src/jsMain/kotlin/modell/SpannerClient.kt
@@ -8,51 +8,26 @@ import io.ktor.http.*
 import kotlinx.serialization.json.JsonObject
 
 
-class SpannerClient {
-    private val host = Localhost()
+class SpannerClient(private val apiPort: Int = 9000) {
     private val client = HttpClient {
         install(JsonFeature) {
             serializer = KotlinxSerializer()
         }
     }
 
-    suspend fun hentPersonMedFnr(fnr: String = "12020052345"): JsonObject {
-        return client.request("$host/api/person-fnr") {
-            method = HttpMethod.Get
-            contentType(ContentType.Application.Json)
-            accept(ContentType.Application.Json)
-            header("fnr", fnr)
-        }
+    suspend fun hentPersonMedFnr(fnr: String) = client.request<JsonObject>("/api/person-fnr") {
+        port = apiPort
+        method = HttpMethod.Get
+        contentType(ContentType.Application.Json)
+        accept(ContentType.Application.Json)
+        header("fnr", fnr)
     }
 
-    suspend fun hentPersonMedAktørId(aktørId: String = "42"): JsonObject {
-        return client.request("$host/api/person-aktorid") {
-            method = HttpMethod.Get
-            contentType(ContentType.Application.Json)
-            accept(ContentType.Application.Json)
-            header("aktorId", aktørId)
-        }
+    suspend fun hentPersonMedAktørId(aktørId: String) = client.request<JsonObject>("/api/person-aktorid") {
+        port = apiPort
+        method = HttpMethod.Get
+        contentType(ContentType.Application.Json)
+        accept(ContentType.Application.Json)
+        header("aktorId", aktørId)
     }
-}
-
-
-internal abstract class Host {
-    abstract fun hostname(): String
-    abstract fun port(): Int
-    abstract fun protocol(): String
-    override fun toString() = "${protocol()}://${this.toShortString()}"
-    fun toShortString() = "${hostname()}:${port()}"
-}
-
-internal class Localhost(private val port: Int = 9000) : Host() {
-    override fun hostname() = "localhost"
-    override fun port() = port
-    override fun protocol() = "http"
-    override fun toString() = "${protocol()}://${hostname()}:${port()}"
-}
-
-internal class Deployed(private val env: Map<String, String>) : Host() {
-    override fun hostname() = env.getValue("HOST")
-    override fun port() = env.getValue("HTTP_PORT").toInt()
-    override fun protocol() = "https"
 }
