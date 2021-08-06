9afde7347e1d138f46ae00ae1c8f3fa0cd8c8d22
Oscar Vladau-Husevold <oscar.vladau-husevold@nav.no>
2021-08-06 14:11:50 +0200

    Fjerner portnummer fra azureAD callback url

--- spanner-backend/src/jvmMain/kotlin/AzureAdAppAuthentication.kt
+++ spanner-backend/src/jvmMain/kotlin/AzureAdAppAuthentication.kt
@@ -1,15 +1,12 @@
 import io.ktor.application.*
 import io.ktor.auth.*
-import io.ktor.auth.jwt.*
-import io.ktor.client.*
-import io.ktor.http.*
 
 internal val AZURE_OAUTH = "azure_oauth"
 
 internal fun Application.azureAdAppAuthentication(azureAdClient: IAzureAdClient, host: Host) {
     install(Authentication) {
         oauth(AZURE_OAUTH) {
-            urlProvider = { "$host/oauth2/callback" }
+            urlProvider = { "{${host.protocol()}://{${host.hostname()}/oauth2/callback" }
             providerLookup = {
                 azureAdClient.configuration()
             }
