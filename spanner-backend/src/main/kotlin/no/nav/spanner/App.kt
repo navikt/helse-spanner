090cfdfd96adfdfff256bde48eb92d59cb1bf35b
Erlend Viddal <eviddal@gmail.com>
2021-08-24 13:56:53 +0200

    Logger exception i oppstart

--- spanner-backend/src/main/kotlin/no/nav/spanner/App.kt
+++ spanner-backend/src/main/kotlin/no/nav/spanner/App.kt
@@ -1,7 +1,14 @@
 package no.nav.spanner
 
 import io.ktor.server.netty.*
+import no.nav.spanner.Log.Companion.logger
 
 fun main() {
-    EngineMain.main(emptyArray())
+    try {
+        EngineMain.main(emptyArray())
+    } catch (err: Throwable) {
+        logger("App")
+            .exception(err)
+            .error("Feilet i oppstart")
+    }
 }
