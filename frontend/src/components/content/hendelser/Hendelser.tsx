767adfc1ec32c2648abbf35a5bdd635dd6a21525
Erlend Viddal <eviddal@gmail.com>
2021-09-17 12:54:52 +0200

    Storterer hendelseliste basert på dato

--- frontend/src/components/content/hendelser/Hendelser.tsx
+++ frontend/src/components/content/hendelser/Hendelser.tsx
@@ -1,11 +1,37 @@
-import React from "react";
-import {AktivitetDto, AktivitetsloggDto, KontekstDto} from "../../../state/dto";
-import {useAktivitetslogg} from "../../../state/contexts";
-import {mapNotUndefined} from "../../../utils";
-import styles from "./Hendelser.module.css";
-import {Hendelse} from "./Hendelse";
+import React from 'react'
+import { AktivitetDto, AktivitetsloggDto, KontekstDto } from '../../../state/dto'
+import { useAktivitetslogg } from '../../../state/contexts'
+import { hasValue, mapNotUndefined } from '../../../utils'
+import styles from './Hendelser.module.css'
+import { Hendelse } from './Hendelse'
+import parseISO from 'date-fns/parseISO'
+import compareAsc from 'date-fns/compareAsc'
 
-export const Hendelser = React.memo(({aktiviteter}: { aktiviteter: AktivitetDto[] }) => {
+type BeriketKontekst = {
+    kontekst: KontekstDto
+    aktiviteter: AktivitetDto[]
+    opprettet: Date
+}
+const berikKontekster = (
+    alleAktiviteter: AktivitetDto[],
+    hendelseKontektster: [KontekstDto, number][]
+): BeriketKontekst[] => {
+    return hendelseKontektster.map(([kontekst, index]: [KontekstDto, number]) => {
+        const aktiviteter = alleAktiviteter.filter((aktivitet) => aktivitet.kontekster.includes(index))
+        const opprettet =
+            aktiviteter
+                .map((it) => parseISO(it.tidsstempel))
+                .sort(compareAsc)
+                .find(hasValue) ?? parseISO('1900-01-01')
+        return {
+            kontekst,
+            aktiviteter,
+            opprettet,
+        }
+    })
+}
+
+export const Hendelser = React.memo(({ aktiviteter }: { aktiviteter: AktivitetDto[] }) => {
     const kontekstFinnesiAktiviteter = (kontekst: KontekstDto, index: number): boolean =>
         !!aktiviteter.find((aktivitet) => aktivitet.kontekster.includes(index))
 
@@ -14,19 +40,23 @@ export const Hendelser = React.memo(({aktiviteter}: { aktiviteter: AktivitetDto[
 
     const aktivitetslogg = useAktivitetslogg()
 
-    const hendelseKontektster: [KontekstDto, number][] = mapNotUndefined(aktivitetslogg.kontekster, (kontekst, index) => {
-        if (kontekstFinnesiAktiviteter(kontekst, index) && erMeldingsKontekst(kontekst)) {
-            return [kontekst, index]
-        } else return undefined
-    })
-
+    const hendelseKontektster: [KontekstDto, number][] = mapNotUndefined(
+        aktivitetslogg.kontekster,
+        (kontekst, index) => {
+            if (kontekstFinnesiAktiviteter(kontekst, index) && erMeldingsKontekst(kontekst)) {
+                return [kontekst, index]
+            } else return undefined
+        }
+    )
+    const berikedeKontekster = berikKontekster(aktiviteter, hendelseKontektster).sort((a, b) =>
+        compareAsc(a.opprettet, b.opprettet)
+    )
     return (
         <div className={styles.Hendelser}>
-            {hendelseKontektster.map(([kontekst, index]) => {
-                const kontekstAktiviter = aktiviteter.filter((aktivitet) => aktivitet.kontekster.includes(index))
+            {berikedeKontekster.map(({ kontekst, aktiviteter }) => {
                 return (
                     <Hendelse
-                        aktiviteter={kontekstAktiviter}
+                        aktiviteter={aktiviteter}
                         kontekst={kontekst}
                         key={kontekst.kontekstMap.meldingsreferanseId}
                     />
--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -4,3 +4,7 @@ export const mapNotUndefined = <T, Y>(array: T[], mapping: (kontekst: T, index: 
         .map(([element, index]) => mapping(element, index))
         .filter(y => y != undefined) as Y[]
 }
+
+//https://stackoverflow.com/questions/2559318/how-to-check-for-an-undefined-or-null-variable-in-javascript
+export const hasValue = (it: any) =>
+     it != null
