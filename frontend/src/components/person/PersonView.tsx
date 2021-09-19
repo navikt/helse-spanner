346d564271c87e709b2bb9ae2fe103dabbc036aa
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2021-09-19 20:55:42 +0200

    Sett de tre hoved elementene for Spanner i Card elementer

--- frontend/src/components/person/PersonView.tsx
+++ frontend/src/components/person/PersonView.tsx
@@ -4,13 +4,16 @@ import classNames from 'classnames'
 import styles from './PersonView.module.css'
 import { PersonTree } from '../tree/PersonTree'
 import { Content } from '../content/Content'
+import {Card, HeaderCard} from "../Card";
 
 export const PersonView = React.memo(() => {
     return (
         <div className={classNames(styles.PersonView)} data-testid="person">
-            <PersonHeader />
-            <PersonTree />
-            <Content />
+            <HeaderCard style={{gridArea: "header"}}><PersonHeader /></HeaderCard>
+            <Card style={{width: "80%", height: "100%"}}><PersonTree /></Card>
+            <Card style={{height: "100%"}}>
+                <Content />
+            </Card>
         </div>
     )
 })
