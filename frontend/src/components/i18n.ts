d8c723eddca5a970f80f2cc5d4167ef18119adee
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-02-02 13:11:42 +0100

    🐟 retter bug for utbetalinger uten dato
    Utbetalinger uten utbetaling har ofte FOM: -999999999-01-01 og
    TOM: +999999999-12-31. Burde kanskje heller vises som "Ingen dato" enn
    "[ugyldig dato] - [ugyldig dato]"
    
    Co-authored-by: Sindre Broch <sindre.broch@nav.no>

--- frontend/src/components/i18n.ts
+++ frontend/src/components/i18n.ts
@@ -1,4 +1,11 @@
 import { format } from 'date-fns'
 import parseISO from 'date-fns/parseISO'
 
-export const somNorskDato = (dato: string) => format(parseISO(dato), 'dd.MM.yyyy')
+export const somNorskDato = (dato: string) => {
+    try {
+        const date = parseISO(dato)
+        return format(date, 'dd.MM.yyyy')
+    } catch (e) {
+        return "[ugyldig dato]"
+    }
+}
