f024473134fcd53e917d420ef8d37f95b0b4ef14
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-02-03 15:56:53 +0100

    Lenke til sporing for forkastede vedtaksperioder

--- frontend/src/components/content/IngressView.tsx
+++ frontend/src/components/content/IngressView.tsx
@@ -2,24 +2,33 @@ import React from "react";
 import {ContentCategory} from "./ContentCategory";
 import {ContentView} from "../../state/state";
 import {Hendelser} from "./hendelser/Hendelser";
-import {useVedtak} from "../../state/contexts";
+import {useForkastetVedtaksperiode, useVedtak} from "../../state/contexts"
 
-const Vedtaksperiode = React.memo(() => {
-    const vedtaksperiode = useVedtak()
+const _ingressView = (vedtaksperiodeId: string) => {
     const sporing = window.location.origin.includes("dev") ? 'https://sporing.dev.intern.nav.no' : 'https://sporing.intern.nav.no'
     return (
         <div>
-            <a href={`${sporing}/tilstandsmaskin/${vedtaksperiode.id}`} target="_blank">Sporing</a>
+            <a href={`${sporing}/tilstandsmaskin/${vedtaksperiodeId}`} target="_blank">Sporing</a>
         </div>
     )
+}
+const Vedtaksperiode = React.memo(() => {
+    const vedtaksperiode = useVedtak()
+    return _ingressView(vedtaksperiode.id)
 })
 Vedtaksperiode.displayName = 'IngressView.Vedtaksperiode'
+
+const ForkastetVedtaksperiode = React.memo(() => {
+    const vedtaksperiode = useForkastetVedtaksperiode()
+    return _ingressView(vedtaksperiode.id)
+})
+ForkastetVedtaksperiode.displayName = 'IngressView.ForkastetVedtaksperiode'
 
 export const IngressView = React.memo(() => {
     return (
         <ContentCategory
             displayName={ContentView.Ingress}
-            {...{Vedtaksperiode}}
+            {...{Vedtaksperiode, ForkastetVedtaksperiode}}
         />
 
     )
