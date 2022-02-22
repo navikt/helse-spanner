5bfdc7ac48dc173b9be790453499aaa295d5261f
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-02-22 18:55:40 +0100

    Lukker alle hendelser etter fetch av ny person

--- frontend/src/components/person/PersonView.tsx
+++ frontend/src/components/person/PersonView.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { PersonHeader } from './PersonHeader'
 import classNames from 'classnames'
 import styles from './PersonView.module.css'
 import { PersonTree } from '../tree/PersonTree'
 import { Content } from '../content/Content'
 import { Card, HeaderCard } from '../Card'
+import { useResetRecoilState } from 'recoil'
+import { åpneHendelseDokumentState } from '../../state/state'
+import { usePerson } from '../../state/contexts'
 
 export const PersonView = React.memo(() => {
+    const resetÅpneHendelser = useResetRecoilState(åpneHendelseDokumentState)
+    const person = usePerson()
+    useEffect(() => {
+        resetÅpneHendelser()
+    }, [person])
+
     return (
         <div className={classNames(styles.PersonView)} data-testid="person">
             <HeaderCard style={{ gridArea: 'header' }}>
