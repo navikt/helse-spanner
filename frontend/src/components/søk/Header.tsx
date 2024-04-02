0ad6dd0210d8b96b3ac1580a45345226451f44f5
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-04-02 21:04:18 +0200

    💄 Hvit tekst for navn på innlogget bruker

--- frontend/src/components/søk/Header.tsx
+++ frontend/src/components/søk/Header.tsx
@@ -1,28 +1,25 @@
 import React from 'react'
 import {InternalHeader, Spacer} from "@navikt/ds-react";
 import styles from './Header.module.css'
-import classNames from "classnames";
 import {useQuery} from "react-query";
-import {Spinner} from "../Spinner";
-import {Feilmelding} from "../Feilmelding";
 
 export const Header: React.FC<object> = ({ children }) => {
     const hentBrukerinfo = async() => {
         return await fetch("/api/meg")
             .then(response => response.json())
     }
-    const { isLoading, isError, data, error } = useQuery(['brukerinfo'], hentBrukerinfo)
+    const { isLoading, isError, data } = useQuery(['brukerinfo'], hentBrukerinfo)
 
     const navn = isLoading ? '[laster]' : isError ? '[feil]' : data.navn
     const ident = isLoading ? '[laster]' : isError ? '[feil]' : data.ident
     return (
         <InternalHeader data-theme="spanner">
             <InternalHeader.Title href="/">
-                <span className={classNames(styles.SpannSpan)}>🪣</span>er
+                <span className={styles.SpannSpan}>🪣</span>er
             </InternalHeader.Title>
             {children}
             <Spacer/>
-            <InternalHeader.User name={ navn } description={ ident }/>
+            <InternalHeader.User className={styles.User} name={ navn } description={ ident }/>
         </InternalHeader>
     )
 }
