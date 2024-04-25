ed42c305b648a92383ed8f7122e050bdce5824ca
david steinsland <david@davidsteinsland.net>
2024-04-25 07:43:15 +0200

    bytter ut sporinglinker

--- frontend/src/components/tree/links.ts
+++ frontend/src/components/tree/links.ts
@@ -3,8 +3,8 @@ import { Environment } from '../../external/environment'
 const sporingHostname = Environment.isDevelopment
     ? 'localhost'
     : window.location.origin.includes('dev')
-    ? 'sporing.intern.dev.nav.no'
-    : 'sporing.intern.nav.no'
+    ? 'sporing.ansatt.dev.nav.no'
+    : 'sporing.ansatt.nav.no'
 export const tilstandsmaskinSporingUrl = (id: string) => `https://${sporingHostname}/tilstandsmaskin/${id}`
 export const personSporingUrl = (aktørId: string) => `https://${sporingHostname}/person/${aktørId}`
 
