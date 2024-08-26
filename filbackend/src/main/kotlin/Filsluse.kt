aab22ab565ffecd6e183f3b7a9ed7c1b4853f4b8
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-08-26 11:32:14 +0200

    En ørliten env-variabel for å si hvor filene er
    Co-authored-by: David Steinsland <david@davidsteinsland.net>

--- filbackend/src/main/kotlin/Filsluse.kt
+++ filbackend/src/main/kotlin/Filsluse.kt
@@ -3,7 +3,7 @@ import java.util.UUID
 
 object Filsluse {
     private fun String.innhold() = File(this).takeIf { it.exists() }?.readText()
-    private fun spannerRoot() = "${System.getenv("HOME")}/.spanner"
+    private fun spannerRoot() = System.getenv("SPANNER_ROOT").takeUnless { it.isNullOrBlank() } ?: "${System.getenv("HOME")}/.spanner"
     fun UUID.finnPerson() = "${spannerRoot()}/personer/$this.json".innhold()
     fun UUID.finnHendelse() = "${spannerRoot()}/hendelser/$this.json".innhold()
 }
