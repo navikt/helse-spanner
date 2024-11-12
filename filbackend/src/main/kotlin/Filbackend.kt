ca28416995519d001fcd844d81ca03bbf29521b5
david steinsland <david@davidsteinsland.net>
2024-11-12 19:59:26 +0100

    fikser spannerish

--- filbackend/src/main/kotlin/Filbackend.kt
+++ filbackend/src/main/kotlin/Filbackend.kt
@@ -14,61 +14,63 @@ import io.ktor.server.routing.*
 import java.util.UUID
 
 fun main() {
-    embeddedServer(CIO, environment = applicationEngineEnvironment {
+    embeddedServer(
+        factory = CIO,
+        environment = applicationEnvironment {},
+        configure = {
+            connector {
+                port = 3000
+            }
+        }
+    ) {
         // Mediumenes rekkefølge er _ikke_ likegyldig. De er i prioritert rekkefølge
         val hentingsmedium = listOf(Minne, Fil)
         val lagringsmedium = listOf<Lagringsmedium>(Minne)
 
-        module {
-            routing {
-                frontendRouting()
+        routing {
+            frontendRouting()
 
-                get("/api/meg") {
-                    call.respondText("""{ "navn": "Viggo Velferdsvenn", "ident": "V1660" } """, Json, OK)
-                }
+            get("/api/meg") {
+                call.respondText("""{ "navn": "Viggo Velferdsvenn", "ident": "V1660" } """, Json, OK)
+            }
 
-                get("/api/person/") {
-                    val maskertId = call.uuidHeader("maskertId") ?: return@get call.respond(BadRequest)
-                    val person = hentingsmedium.hentPerson(maskertId) ?: return@get call.respond(NotFound)
-                    call.respondText(person, Json, OK)
-                }
+            get("/api/person/") {
+                val maskertId = call.uuidHeader("maskertId") ?: return@get call.respond(BadRequest)
+                val person = hentingsmedium.hentPerson(maskertId) ?: return@get call.respond(NotFound)
+                call.respondText(person, Json, OK)
+            }
 
-                post("/api/uuid/") {
-                    val maskertId = call.uuidHeader("maskertId") ?: return@post call.respond(BadRequest)
-                    val person = hentingsmedium.hentPerson(maskertId) ?: return@post call.respond(NotFound)
-                    call.respondText(person, Json, OK)
-                }
+            post("/api/uuid/") {
+                val maskertId = call.uuidHeader("maskertId") ?: return@post call.respond(BadRequest)
+                val person = hentingsmedium.hentPerson(maskertId) ?: return@post call.respond(NotFound)
+                call.respondText(person, Json, OK)
+            }
 
-                get("/api/hendelse/{meldingsreferanse}") {
-                    val meldingsreferanse = call.uuidParameter("meldingsreferanse") ?: return@get call.respond(BadRequest)
-                    val hendelse = hentingsmedium.hentHendelse(meldingsreferanse) ?: return@get call.respond(NotFound)
-                    call.respondText(hendelse, Json, OK)
-                }
+            get("/api/hendelse/{meldingsreferanse}") {
+                val meldingsreferanse = call.uuidParameter("meldingsreferanse") ?: return@get call.respond(BadRequest)
+                val hendelse = hentingsmedium.hentHendelse(meldingsreferanse) ?: return@get call.respond(NotFound)
+                call.respondText(hendelse, Json, OK)
+            }
 
-                post("/api/person/{uuid}") {
-                    val uuid = call.uuidParameter("uuid") ?: return@post call.respond(BadRequest)
-                    val person = call.receiveText()
-                    lagringsmedium.lagrePerson(uuid, person)
-                    call.respond(Created)
-                }
+            post("/api/person/{uuid}") {
+                val uuid = call.uuidParameter("uuid") ?: return@post call.respond(BadRequest)
+                val person = call.receiveText()
+                lagringsmedium.lagrePerson(uuid, person)
+                call.respond(Created)
+            }
 
-                post("/api/hendelse/{uuid}") {
-                    val uuid = call.uuidParameter("uuid") ?: return@post call.respond(BadRequest)
-                    val hendelse = call.receiveText()
-                    lagringsmedium.lagreHendelse(uuid, hendelse)
-                    call.respond(Created)
-                }
+            post("/api/hendelse/{uuid}") {
+                val uuid = call.uuidParameter("uuid") ?: return@post call.respond(BadRequest)
+                val hendelse = call.receiveText()
+                lagringsmedium.lagreHendelse(uuid, hendelse)
+                call.respond(Created)
+            }
 
-                get("/internal/*") {
-                    call.respond(OK)
-                }
+            get("/internal/*") {
+                call.respond(OK)
             }
         }
-
-        connector {
-            port = 3000
-        }
-    }).start(true)
+    }.start(true)
 }
 
 private fun ApplicationCall.uuidHeader(navn: String) = request.headers[navn]?.let { UUID.fromString(it) }
