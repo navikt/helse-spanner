016f646099460ec590b214dc685794223c8f5e74
david steinsland <david@davidsteinsland.net>
2025-01-27 13:10:51 +0100

    bumper tbd-libs

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -4,7 +4,7 @@ val logbackClassicVersion = "1.5.12"
 val logbackEncoderVersion = "8.0"
 val junitJupiterVersion = "5.11.3"
 val ktorVersion = "3.0.1"
-val tbdLibsVersion = "2024.11.25-10.59-6f263a10"
+val tbdLibsVersion = "2025.01.27-12.50-76316f3b"
 
 plugins {
     kotlin("jvm") version "2.0.21"
